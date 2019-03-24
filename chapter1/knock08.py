bc404b494355cff3d82b9edbf373621ae70bbf45
gori-san <zer0193yamyam@gmail.com>
2019-03-24 10:55:54 +0900

    add knock08,09

---
+++ chapter1/knock08.py
@@ -0,0 +1,17 @@
+
+
+def cipher(str):
+    result = ""
+    for c in str:
+        if c.islower():
+            result += chr(219 - ord(c))
+        else:
+            result += c
+
+    return result
+
+
+if __name__ == "__main__":
+    str = "I am a NLPer"
+    print(cipher(str))
+    print(cipher(cipher(str)))
