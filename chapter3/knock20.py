11e50edafdc679ea4811847cf448aebbb26194c8
gori-san <zer0193yamyam@gmail.com>
2019-04-16 10:49:56 +0900

    add knock20

--- chapter3/knock20.py
+++ chapter3/knock20.py
@@ -4,8 +4,8 @@ import json
 
 with gzip.open(sys.argv[1],'rt',encoding='utf-8') as input_file:
     for line in input_file:
-        json_dic = json.loads(line)
-        if json_dic['title'] == 'イギリス':
-            print(json_dic['text'])
+        json_input = json.loads(line)
+        if json_input['title'] == 'イギリス':
+            print(json_input['text'])
             with open('Britain.txt','w',encoding='utf-8') as output_file:
-                output_file.write(json_dic['text'])
+                output_file.write(json_input['text'])
