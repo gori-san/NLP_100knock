e5f7242f89a9c495d1309c5604a8f051c3df811b
gori-san <zer0193yamyam@gmail.com>
2019-06-03 16:28:42 +0900

    add knock72

---
+++ chapter8/knock72.py
@@ -0,0 +1,40 @@
+import numpy as np
+from sklearn.externals import joblib
+from sklearn.feature_extraction.text import TfidfVectorizer
+from knock71 import is_stopword
+import snowballstemmer
+from tqdm import tqdm
+
+
+def get_feature_base(sentence):
+    stemmer = snowballstemmer.stemmer('english')
+    words = sentence.split()
+    result = []
+    for word in words:
+        if is_stopword(word):
+            continue
+        result.append(stemmer.stemWord(word))
+    return ' '.join(result)
+
+
+def create_feature():
+    labels = []
+    corpus = []
+    with open('sentiment.txt', 'r', encoding='utf-8') as senti_file:
+        for line in tqdm(senti_file):
+            label = line[:2]
+            sentence = line.rstrip()[3:]
+            labels.append(int(label))
+            sentence_ = get_feature_base(sentence)
+            corpus.append(sentence_)
+
+        vectorizer = TfidfVectorizer()
+        feature = vectorizer.fit_transform(corpus).toarray()
+        sentiment = np.array(labels)
+
+        joblib.dump(feature, 'feature')
+        joblib.dump(sentiment, 'sentiment')
+
+
+if __name__ == '__main__':
+    create_feature()
